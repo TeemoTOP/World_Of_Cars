1f5aa5791a638206c473ef96c8e0ed8eb813a7ca
Igor2002-likeBMW <149398066+Igor2002-likeBMW@users.noreply.github.com>
2024-03-16 18:10:48 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    var paragraphs = document.getElementsByTagName('p');
+    for (var i = 0; i < paragraphs.length; i++) {
+        var text = paragraphs[i].textContent;
+        paragraphs[i].innerHTML = capitalizeSentences(text);
+    }
+};
+
+function capitalizeSentences(text) {
+    return text.replace(/(^|\.\s+)([a-z])/g, function(match) {
+        return match.toUpperCase();
+    });
+}
