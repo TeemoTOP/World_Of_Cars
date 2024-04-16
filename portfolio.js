32d3e12d8a5eb10fca45ea05f51c799a05901b0b
TeemoTOP <96535585+TeemoTOP@users.noreply.github.com>
2024-04-16 19:00:51 +0200

    Update portfolio.js
    Dodano komentarze

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,4 @@
+// Funkcja wywołująca po załadowaniu okna przeglądarki
 window.onload = function() {
     var paragraphs = document.getElementsByTagName('p');
     for (var i = 0; i < paragraphs.length; i++) {
@@ -6,6 +7,7 @@ window.onload = function() {
     }
 };
 
+// Funkcja, która zamienia pierwsze litery każdego zdania na duże litery
 function capitalizeSentences(text) {
     return text.replace(/(^|\.\s+)([a-z])/g, function(match) {
         return match.toUpperCase();
